4f032b09bc980f406568bc4829c6e226a99a2cbf
SoNu <SoNu@192.168.0.103>
2017-07-21 00:02:30 +0530

    final changes POC

--- springTemplate/src/main/java/hello/GreetingController.java
+++ springTemplate/src/main/java/hello/GreetingController.java
@@ -1,7 +1,8 @@
 package hello;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name", name);
         return "greeting";
     }
 
